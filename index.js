af7bede1c9f34985cfe2712bdbec3d473d8941d6
NahidAhmed07 <mdnahidahmed2002@gmail.com>
2021-10-28 23:25:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Assignment Server Running");
+});
+
+app.listen(port, () => {
+  console.log("volunteer server running on port", port);
+});
